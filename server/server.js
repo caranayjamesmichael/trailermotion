449b37fa4f18037c420ab08037dbeb24de0bea32
caranayjamesmichael <caranayjamesmichaelprogramming@gmail.com>
2023-07-17 21:19:47 +0800

    Implemented cors and added shows routes for data fetching

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from "express";
+import cors from "cors";
 import showsRoutes from "./routes/showsRoutes.js";
 
 const PORT = 8000;
 
 const app = express();
 
-app.use("/api", showsRoutes)
+app.use(cors());
+
+app.use("/api", showsRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
