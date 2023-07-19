e0ba48b9de0c68bab48dda87c1c2cc1a63711a21
caranayjamesmichael <caranayjamesmichaelprogramming@gmail.com>
2023-07-19 16:52:58 +0800

    Added positioning adjustments and  navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,33 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className=" flex justify-between items-center px-5 bg-[#171717] h-20">
+    <div className="flex justify-between items-center px-5 h-20 absolute top-0 left-0 right-0 bottom-0">
       <h2 className="text-[2rem] text-white">Trailermotion</h2>
+
+      <ul className="flex justify-around gap-6">
+        <li>
+          <a className="text-white" href="#">Home</a>
+        </li>
+        <li>
+          <a className="text-white" href="#">Movies</a>
+        </li>
+        <li>
+          <a className="text-white" href="#">TV Shows</a>
+        </li>
+        <li>
+          <a className="text-white" href="#">Categories</a>
+        </li>
+        <li>
+          <a className="text-white" href="#">Pricing</a>
+        </li>
+        <li>
+          <a className="text-white" href="#">About</a>
+        </li>
+      </ul>
 
       <form>
         <input
-          className="bg-transparent w-64 p-1"
+          className=" bg-gray-900 w-64 px-2 py-2 text-white outline-none"
           type="text"
           placeholder="Search here..."
         ></input>
