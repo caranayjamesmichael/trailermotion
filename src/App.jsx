35b47511aaf2433fa962cb6f8a921420dd9ee877
caranayjamesmichael <caranayjamesmichaelprogramming@gmail.com>
2023-07-24 22:47:31 +0800

    Added route for genres

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Movies from "./pages/Movies";
 import Series from "./pages/Series";
 import TopRated from "./pages/TopRated";
 import Trailer from "./pages/Trailer";
+import Genre from "./pages/Genre";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route exact path="/movies" element={<Movies />}></Route>
           <Route path="/series" element={<Series />}></Route>
           <Route path="/toprated" element={<TopRated />}></Route>
+          <Route path="/genres" element={<Genre />}></Route>
           <Route path="/trailer" element={<Trailer />}></Route>
         </Routes>
       </BrowserRouter>
