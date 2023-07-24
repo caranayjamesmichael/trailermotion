4274b6500e5f78efa0807c4bfacf55fb0c420ae3
caranayjamesmichael <caranayjamesmichaelprogramming@gmail.com>
2023-07-24 19:12:53 +0800

    Created ShowItem component for each shows so they can have their own state in playing the video

---
+++ src/components/ShowItem.jsx
@@ -0,0 +1,68 @@
+import React, { useRef, useState } from "react";
+
+const ShowItem = ({ show }) => {
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <div className="rounded-t-3xl shadow-xl text-white relative">
+      {hovered ? (
+        <video
+          className="w-80 h-80 object-cover rounded-t-3xl"
+          autoPlay
+          controls
+          src={`../../server/models/${show.trailerURL}`}
+        ></video>
+      ) : (
+        <img
+          className="w-80 h-80 object-cover rounded-t-3xl"
+          muted
+          autoPlay
+          src={show.imageURL}
+        ></img>
+      )}
+
+      <div className={`flex p-5 bg-[rgba(37, 38, 45, 0.8)]`}>
+        <div className=" w-3/4">
+          <h4>
+            <b>
+              <strong>{show.title}</strong>
+            </b>
+          </h4>
+
+          <div className="flex ml-[-8px]">
+            <p className="mt-3 bg-[rgba(30,30,36,255)] px-2 py-0.5 shadow-2xl rounded">
+              <small>{"Release Date: " + show.releaseDate}</small>
+            </p>
+          </div>
+
+          <div className="flex gap-2">
+            <p className="mt-3  bg-[rgba(30,30,36,255)] px-2 py-0.5 shadow-black shadow rounded">
+              <small>{show.genre}</small>
+            </p>
+            <p className="mt-3  bg-[rgba(30,30,36,255)] px-2 py-0.5 shadow-black shadow rounded">
+              <small>{show.type}</small>
+            </p>
+            <p className="mt-3  bg-[rgba(30,30,36,255)] px-2 py-0.5 shadow-black shadow rounded">
+              <small>{show.ratings + " ⭐"}</small>
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col justify-end items-center w-1/4">
+          <button
+            className={
+              hovered
+                ? "bg-green-600 px-5 py-3 rounded-md "
+                : "bg-red-600 px-5 py-3 rounded-md "
+            }
+            onMouseDown={() => setHovered((prev) => !prev)}
+            href="#"
+          >
+            {hovered ? "Stop" : "Watch"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShowItem;
