7f6db7d02ab976ddd60467a6a5be9b37de11b703
caranayjamesmichael <caranayjamesmichaelprogramming@gmail.com>
2023-07-24 19:12:17 +0800

    Updated the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Navbar = () => {
+import logo from "../assets/images/trailermotion.png";
+import homeIcon from "../assets/images/home.png";
+import movieIcon from "../assets/images/movie-clapper-open.png";
+import showIcon from "../assets/images/tv.png";
+import starIcon from "../assets/images/star.png";
+import logoutIcon from "../assets/images/logout.png";
+import dropdown from "../assets/images/down.png";
+
+const Navbar = ({ isUserLoggedIn, setIsUserLoggedIn }) => {
+  const [dropdownStatus, setDropdownStatus] = useState(false);
+
+  const handleDropdown = (e) => {
+    setDropdownStatus((prevState) => !prevState);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    isUserLoggedIn ? setIsUserLoggedIn(false) : setIsUserLoggedIn(true);
+  };
+
   return (
-    <div className="flex justify-between items-center px-5 h-20 absolute top-0 left-0 right-0 bottom-0">
-      <h2 className="text-[2rem] text-white">Trailermotion</h2>
+    <div className="flex-col l:flex xl:flex justify-around items-center w-screen lg:h-screen lg:w-72 bg-[rgba(30,30,36,255)] shadow-lg sticky top-0 z-10">
+      <div
+        className={`${
+          dropdownStatus ? "" : "hidden"
+        } flex lg:flex py-10 lg:py-0 w-full justify-center`}
+      >
+        <img className="w-48" src={logo}></img>
+      </div>
 
-      <ul className="flex justify-around gap-6">
-        <li>
-          <a className="text-white" href="#">Home</a>
-        </li>
-        <li>
-          <a className="text-white" href="#">Movies</a>
-        </li>
-        <li>
-          <a className="text-white" href="#">TV Shows</a>
-        </li>
-        <li>
-          <a className="text-white" href="#">Categories</a>
-        </li>
-        <li>
-          <a className="text-white" href="#">Pricing</a>
-        </li>
-        <li>
-          <a className="text-white" href="#">About</a>
-        </li>
-      </ul>
+      <div className={`${dropdownStatus ? "" : "hidden"} lg:block w-full`}>
+        <ul>
+          <li className="flex flex-col gap-5 py-3">
+            <h2 className="px-14 text-[#676a7c] font-semibold">
+              <small>Menu</small>
+            </h2>
+            <a
+              className="flex items-center text-[#676a7c] hover:border-l-4 hover:border-red-500 px-14 duration-200 font-bebas"
+              href="/"
+            >
+              <img className="w-5" src={homeIcon}></img>
+              <p className="px-3">Home</p>
+            </a>
+            <a
+              className="flex items-center text-[#676a7c] hover:border-l-4 hover:border-red-500 px-14 duration-200 font-bebas"
+              href="/movies"
+            >
+              <img className="w-5" src={movieIcon}></img>
+              <p className="px-3">Movies</p>
+            </a>
+            <a
+              className="flex items-center text-[#676a7c] hover:border-l-4 hover:border-red-500 px-14 duration-200 font-bebas"
+              href="/series"
+            >
+              <img className="w-5" src={showIcon}></img>
+              <p className="px-3">TV Shows</p>
+            </a>
+            <a
+              className="flex items-center text-[#676a7c] hover:border-l-4 hover:border-red-500 px-14 duration-200 font-bebas"
+              href="#"
+            >
+              <img className="w-5" src={showIcon}></img>
+              <p className="px-3">Genre</p>
+            </a>
+            <a
+              className="flex items-center text-[#676a7c] hover:border-l-4 hover:border-red-500 px-14 duration-200 font-bebas"
+              href="/toprated"
+            >
+              <img className="w-5" src={starIcon}></img>
+              <p className="px-3">Top Rated</p>
+            </a>
+          </li>
 
-      <form>
-        <input
-          className=" bg-gray-900 w-64 px-2 py-2 text-white outline-none"
-          type="text"
-          placeholder="Search here..."
-        ></input>
-      </form>
+          <li className="flex flex-col gap-5 py-3 ">
+            <h2 className="px-14 text-[#676a7c] font-semibold">
+              <small>General</small>
+            </h2>
+            <a
+              className="flex items-center text-[#676a7c] hover:border-l-4 hover:border-red-500 px-14 duration-200 font-bebas"
+              href="#"
+            >
+              <img className="w-5" src={homeIcon}></img>
+              <p className="px-3">Settings</p>
+            </a>
+            <a
+              className="flex items-center text-[#676a7c] hover:border-l-4 hover:border-red-500 px-14 duration-200 font-bebas"
+              href="#"
+              onClick={handleLogin}
+            >
+              <img className="w-5" src={logoutIcon}></img>
+              <p className="px-3">{isUserLoggedIn ? "Logout" : "Login"}</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className={`${dropdownStatus ? "" : "hidden"} lg:block w-full py-9 lg:py-0`}>
+        <p className="text-[#676a7c] text-center">
+          <small>© Trailermotion. 2023</small>
+        </p>
+        <p className="text-[#676a7c] text-center">
+          <small>All Rights Reserved.</small>
+        </p>
+      </div>
+
+      <div className="lg:hidden flex justify-center items-center">
+        <button onClick={handleDropdown}>
+          <img className="w-10" src={dropdown}></img>
+        </button>
+      </div>
     </div>
   );
 };
