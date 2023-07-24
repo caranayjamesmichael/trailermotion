b196b102712b974e6b55e7589a3bce5c5b1745bc
caranayjamesmichael <caranayjamesmichaelprogramming@gmail.com>
2023-07-24 19:14:10 +0800

    Created Series component to render series filtered

---
+++ src/pages/Series.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import Navbar from "../components/Navbar";
+import Shows from "../components/Shows";
+
+const Series = () => {
+  const [shows, setShows] = useState([]);
+
+  useEffect(() => {
+    const getAllSeries = async () => {
+      const response = await axios.get(
+        "http://localhost:8000/api/shows/type/Series"
+      );
+      setShows(response.data);
+    };
+
+    getAllSeries();
+  }, []);
+
+  return (
+    <div className="flex bg-[rgba(30,30,36,255)]">
+      <Navbar />
+      <div className="px-10 py-10">
+        <h2 className="font-bebas text-[2.5rem] text-white">TV Show Trailers</h2>
+        <Shows shows={shows}></Shows>
+      </div>
+    </div>
+  );
+};
+
+export default Series;
