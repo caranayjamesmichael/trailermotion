db7b849c63b2ff9076aaa575cfd9af3df536fa5b
caranayjamesmichael <caranayjamesmichaelprogramming@gmail.com>
2023-07-24 19:10:25 +0800

    Added routing for the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Movies from "./pages/Movies";
+import Series from "./pages/Series";
+import TopRated from "./pages/TopRated";
+import Trailer from "./pages/Trailer";
 
 const App = () => {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/movies" element={<Movies />}></Route>
+          <Route exact path="/series" element={<Series />}></Route>
+          <Route exact path="/toprated" element={<TopRated />}></Route>
+          <Route exact path="/trailer" element={<Trailer />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
