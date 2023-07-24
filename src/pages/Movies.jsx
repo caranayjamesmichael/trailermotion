bd391da14cb8cc9409adb5a5d3e62f6cd05791b0
caranayjamesmichael <caranayjamesmichaelprogramming@gmail.com>
2023-07-24 22:50:13 +0800

    Added Navbar component

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -20,8 +20,9 @@ const Movies = () => {
 
   return (
     <div className="flex bg-[rgba(30,30,36,255)]">
-      <Navbar
-      />
+      <Navbar />
+
+      
       <div className="w-full min-h-screen px-10 py-10">
         <h2 className="font-bebas text-[2.5rem] text-white">Movie Trailers</h2>
         <Shows shows={shows}></Shows>
