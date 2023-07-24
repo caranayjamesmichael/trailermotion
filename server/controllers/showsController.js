46c71ccb276ac86b6b0423c34d5f7b25617df223
caranayjamesmichael <caranayjamesmichaelprogramming@gmail.com>
2023-07-24 19:03:14 +0800

    Created functions for shows controller
    Created the following:
    - getAllShows
    - getShowByID
    - getShowByGenre
    - getShowByType
    - getShowByTopRatings

--- server/controllers/showsController.js
+++ server/controllers/showsController.js
@@ -1,22 +1,28 @@
-import shows from "../models/shows.js";
+import showsData from "../models/shows.json" assert { type: "json" };
 
 const getAllShows = (req, res) => {
-  res.send(shows);
-  console.log(`All shows has been requested!`);
+  try {
+    res.json(showsData.shows);
+    console.log(`All shows has been requested!`);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const getShowByID = (req, res) => {
   const showID = Number(req.params.id);
 
-  const filteredShow = shows.find((show) => show.id === showID);
-  res.json(filteredShow);
-  console.log(`${filteredShow.title} has been requested!`);
+  const filteredShows = showsData.shows.find((show) => show.id === showID);
+
+  res.json(filteredShows);
 };
 
 const getShowByGenre = (req, res) => {
   const showGenre = req.params.genre;
 
-  const filteredShows = shows.filter((show) => show.genre === showGenre);
+  const filteredShows = showsData.shows.filter(
+    (show) => show.genre === showGenre
+  );
   res.json(filteredShows);
   console.log(`${showGenre} shows has been requested`);
 };
@@ -24,9 +30,22 @@ const getShowByGenre = (req, res) => {
 const getShowByType = (req, res) => {
   const showType = req.params.type;
 
-  const filteredShows = shows.filter((show) => show.type === showType);
+  const filteredShows = showsData.shows.filter(
+    (show) => show.type === showType
+  );
   res.json(filteredShows);
   console.log(`${showType} shows has been requested!`);
 };
 
-export { getAllShows, getShowByID, getShowByGenre, getShowByType };
+const getShowByTopRatings = (req, res) => {
+  const filteredShows = showsData.shows.filter((show) => show.ratings >= 4);
+  res.json(filteredShows);
+};
+
+export {
+  getAllShows,
+  getShowByID,
+  getShowByGenre,
+  getShowByType,
+  getShowByTopRatings,
+};
