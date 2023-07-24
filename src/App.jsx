dfea89da165a078826fe24cd1b49b9c8f3afdd11
caranayjamesmichael <caranayjamesmichaelprogramming@gmail.com>
2023-07-24 20:06:16 +0800

    Revisions for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,10 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
-          <Route path="/movies" element={<Movies />}></Route>
+          <Route
+            path="https://trailermotion.vercel.app/movies"
+            element={<Movies />}
+          ></Route>
           <Route path="/series" element={<Series />}></Route>
           <Route path="/toprated" element={<TopRated />}></Route>
           <Route path="/trailer" element={<Trailer />}></Route>
